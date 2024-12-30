75596e0217ed020a287af3ad27dfbeeb01505ce0
FadiAnd1610 <fadi9844@gmail.com>
2024-12-30 12:53:58 +0200

    nav change

--- frontend/src/componets/NavBar.jsx
+++ frontend/src/componets/NavBar.jsx
@@ -1,23 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../images/logo.png"; // עדכני את הנתיב לתמונה
+import { NavLink } from "react-router-dom";
+import logo from "../images/logo.png";
 
-function Navbar() {
-    return (
-        <nav className="navbar">
-            <Link to="/" className="navbar-logo">
-                <img src={logo} alt="Vista Logo" />
-            </Link>
-            <ul className="navbar-links">
-                <li><Link to="/">VISTA</Link></li>
-                <li><Link to="/About">About</Link></li>
-                <li><Link to="/Gallery">Gallery</Link></li>
-                <li><Link to="/TryMeOnGallery">Try me on Gallery</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
-                <li><Link to="/Fidbek">Fidbek</Link></li>
-            </ul>
-        </nav>
-    );
+export default function NavBar() {
+  return (
+    <>
+      <nav>
+         <ul className="navbar">
+         <img src={logo} alt="Vista Logo" className="navbar-logo" />    
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/gallery">Gallery</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/model_test">Model Test</NavLink>
+          </li>
+          <li className="button">
+            <button>Log In</button>
+            <button>Sign Up</button>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
-
-export default Navbar;
