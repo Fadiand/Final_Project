547c1ba90cac241662ce263950f53b829b68cf61
FadiAnd1610 <fadi9844@gmail.com>
2024-12-30 09:11:18 +0200

    change

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from instagram import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('instagram/', include('instagram.urls')),  # חיבור לנתיבים של האפליקציה
+    path('', views.home, name='home'),  # דף הבית
 ]
