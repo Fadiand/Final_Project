df89aac476516457da96d89fd816ec2a5434a6a4
ניר ברששת <nirbar@nyrs-MacBook-Air.local>
2025-01-11 17:20:43 +0200

    שמירת כל השינויים בענף nir

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -198,9 +198,6 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-
-
