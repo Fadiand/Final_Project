20263e3d993bab718f42d04380cbfbc962d81d5c
FadiAnd1610 <fadi9844@gmail.com>
2025-08-01 20:23:40 +0300

    fix setings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -232,14 +232,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://688907604219758a867839a8--silly-daifuku-ed7821.netlify.app",
+    "https://silly-daifuku-ed7821.netlify.app",  # הכתובת הקבועה
 ]
-
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://688907604219758a867839a8--silly-daifuku-ed7821.netlify.app",
+    "https://silly-daifuku-ed7821.netlify.app",
 ]
 
 
